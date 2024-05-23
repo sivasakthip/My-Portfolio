1ad85190a40959adef2f05f24e6cb964db032ba2
SIVA SAKTHI <147609799+sivasakthip@users.noreply.github.com>
2024-05-23 16:20:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+/*================Scroll Section Active*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
+ScrollReveal({
+    reset:true,
+    distance:'-80px',
+    duration:2000,
+    delay:200
+});
+
+ScrollReveal().reveal('.home-content, .heading',{orgin:'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form',{orgin:'top'});
+ScrollReveal().reveal('.home-content h1, .about-img',{orgin:'left'});
+ScrollReveal().reveal('.home-content p, .about-content',{orgin:'right'});
